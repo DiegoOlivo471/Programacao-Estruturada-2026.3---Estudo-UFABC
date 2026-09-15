dab6cbaead74567f52ffe603c4b0abe45d8cb862
DiegoOlivo471 <diegopaschoal471@gmail.com>
2026-09-15 11:04:00 -0300

    fix: first code on the CodeRunner

--- aula01/arithmetic.c
+++ aula01/arithmetic.c
@@ -1,15 +1,9 @@
 #include "arithmetic.h"
 
-// TODO: Implementar a funcao add
 int add(int a, int b) {
-    (void)a;
-    (void)b;
     return a + b;
 }
 
-// TODO: Implementar a funcao subtract
 int subtract(int a, int b) {
-    (void)a;
-    (void)b;
-    return a-b;
+    return a - b;
 }
